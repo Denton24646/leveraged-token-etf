9badd468d4a8d72262c9a60e41a213d0463b02c3
Denton24646 <Denton24646@gmail.com>
2018-10-07 02:22:27 -0400

    mock ERC20

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var DAIMock = artifacts.require("./DAIMock.sol");
+var KyberMock = artifacts.require("./KyberMock.sol");
+var ZeroExMock = artifacts.require("./ZeroExMock.sol");
+ 
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(DAIMock);
+  deployer.deploy(KyberMock);
+  deployer.deploy(ZeroExMock);
 };
